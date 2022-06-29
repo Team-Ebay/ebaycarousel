1924dbb3261709f114ff4eda5bbfe98cfbe32990
Shatawni <shatawni.johnson@gmail.com>
2022-06-29 18:31:31 -0400

    Deleted embedded files and re-structured the layout of the app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const pool = require("./database/conn.js");
+const pool = require("./database/conn");
 const cors = require("cors");
 const app = express();
 
